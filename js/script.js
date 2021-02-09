17fef1ba9d42a3e45de2122eb470a75fe24072b5
moidodyr <khudyakov_ek@mail.ru>
2021-02-09 12:22:25 +0300

    ci(script.js): refactoring

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
 'use strict';
 
-const isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
+const rundomGame = function () {
+    alert('Угадай число от 1 до 100');
 
-const getRandomInt = function () {
-    let min = Math.ceil(1),
-        max = Math.floor(101);
-    return Math.floor(Math.random() * (max - min)) + min;
-}; 
-
-const randomInt = getRandomInt();
-
-alert('Угадай число от 1 до 100');
-
-const fin = function () {
-    let number = prompt('Введите число: ');
-
-    const resaltFunc = function () {
-        if(number === null) {
-            alert('Игра окончена');
-        } else if (!isNumber(parseFloat(number))) {
-            alert('Это не число. Введите число: ');
-            fin();
-        } else if(parseFloat(number) == randomInt){
-            alert('Поздравляю, Вы угадали!!!');
-        } else if(parseFloat(number) > randomInt){
-            alert('Загаданное число меньше.');
-            fin();
-        } else {
-            alert('Загаданное число больше.');
-            fin();
-        }
+    const isNumber = function (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
     };
 
-    return resaltFunc();
-};
+    const getRandomInt = function () {
+        let min = Math.ceil(1),
+            max = Math.floor(101);
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
 
-fin();
+    const randomInt = getRandomInt();
+
+    const main = function () {
+        let number = prompt('Введите число: ');
+
+        const resaltFunc = function () {
+            if (number === null) {
+                alert('Игра окончена');
+            } else if (!isNumber(parseFloat(number))) {
+                alert('Это не число. Введите число: ');
+                main();
+            } else if (parseFloat(number) == randomInt) {
+                alert('Поздравляю, Вы угадали!!!');
+            } else if (parseFloat(number) > randomInt) {
+                alert('Загаданное число меньше.');
+                main();
+            } else {
+                alert('Загаданное число больше.');
+                main();
+            }
+        };
+
+        return resaltFunc();
+    };
+
+    main();
+};
+rundomGame();
