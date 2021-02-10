c69552e118ad70d40eafa03af88f731c7908d4f3
moidodyr <khudyakov_ek@mail.ru>
2021-02-10 12:13:57 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 'use strict';
 
-const rundomGame = function () {
-    alert('Угадай число от 1 до 100');
 
-    let counter = 10,
-        qsn;
-
+const randomGame = function () {
     const isNumber = function (n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
     };
@@ -16,50 +12,30 @@ const rundomGame = function () {
         return Math.floor(Math.random() * (max - min)) + min;
     };
 
-    const randomInt = getRandomInt();
+    const randomInt = getRandomInt(),
+        introduction = alert('Угадай число от 1 до 100');
 
 
-    const main = function () {
+    const mainFunc = function () {
         let number = prompt('Введите число: ');
 
-        const resaltFunc = function () {
-            if (number === null) {
-                alert('Игра окончена');
-            } else if (counter == 1) {
-                qsn = confirm('Вы не угадали. Количество попыток закончено. Хотите сыгать еще?');
-                if (qsn) {
-                    counter = 10;
-                    main();
-                } else {
-                    alert('Игра окончена');
-                }
-            } else if (!isNumber(parseFloat(number))) {
-                counter--;
-                alert(`Это не число. Осталось ${counter} попыток. Введите число: `);
-                main();
-            } else if (parseFloat(number) == randomInt) {
-                alert('Поздравляю, Вы угадали!!!');
-                qsn = confirm('Вы победитель! Хотите сыгать еще?');
-                if (qsn) {
-                    counter = 10;
-                    main();
-                } else {
-                    alert('Игра окончена');
-                }
-            } else if (parseFloat(number) > randomInt) {
-                counter--;
-                alert(`Загаданное число меньше. Осталось ${counter} попыток.`);
-                main();
-            } else {
-                counter--;
-                alert(`Загаданное число больше. Осталось ${counter} попыток.`);
-                main();
-            }
-        };
+        if (parseFloat(number) == randomInt) {
+            alert('Поздравляю, Вы угадали!!!');
+        } else if (number == null) {
+            alert('Игра окончена!');
+        } else if (!isNumber(parseFloat(number))) {
+            alert('Это не число!');
+            mainFunc();
+        } else if(parseFloat(number) > randomInt){
+            alert('Загаданное число меньше.');
+            mainFunc();
+        } else if(parseFloat(number) < randomInt) {
+            alert('Загаданное число больше.');
+            mainFunc();
+        }
 
-        return resaltFunc();
     };
 
-    main();
+    mainFunc();
 };
-rundomGame();
+randomGame();
